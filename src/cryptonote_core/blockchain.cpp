23405da07b17dca1e1486ea1deb6ca4a084343e4
moneromooo-monero <moneromooo-monero@users.noreply.github.com>
2017-12-13 18:44:18 +0000

    blockchain: don't leave dangling pointers in this

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -495,7 +495,9 @@ bool Blockchain::deinit()
   }
 
   delete m_hardfork;
+  m_hardfork = NULL;
   delete m_db;
+  m_db = NULL;
   return true;
 }
 //------------------------------------------------------------------
